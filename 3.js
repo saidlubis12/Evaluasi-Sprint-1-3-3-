3b0c71d42a909d89227488978303cbad0f82a554
ninjaa <alkhudrisaid83@gmail.com>
2020-09-26 13:39:12 +0700

    first commit

---
+++ 3.js
@@ -0,0 +1,7 @@
+function luas(panjang, lebar) {
+    var luas = panjang * lebar;
+    return luas;
+}
+document.write("panjang = 12 <br>");
+document.write("lebar = 11 <br>");
+document.write("luas persegi panjang :" + " " + luas(12, 11))
